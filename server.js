75fcc6dce56dd47bfcd9274bd3d32e95a5392dc2
Chris <ChristianFigueroaMontano@outlook.com>
2021-01-29 14:17:49 -0600

    Base day picker, modfificacion ubicacion

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
- 
+ //https://api.themoviedb.org/3/movie/3/videos?api_key=a6eb2a522c211c4550bddc7f6fbc25ee&language=en-US
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
